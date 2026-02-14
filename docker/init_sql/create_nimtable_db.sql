b57ea78a7c052d1c7e0828eda5d8336412b7e964
Ansh Sarkar <anshsarkar1@gmail.com>
2026-02-14 02:07:59 -0500

    Bug fix

--- docker/init_sql/create_nimtable_db.sql
+++ docker/init_sql/create_nimtable_db.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE nimtable;
 CREATE USER nimtable_user WITH PASSWORD 'password';
 GRANT ALL PRIVILEGES ON DATABASE nimtable TO nimtable_user;
+
+\c nimtable
+GRANT ALL ON SCHEMA public TO nimtable_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO nimtable_user;
